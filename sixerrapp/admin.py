895513228cb28f2a5c347c7f1dab39f551983e74
ANHTUAN DANG <adang33@gatech.edu>
2019-01-02 03:54:20 -0800

    update

--- sixerrapp/admin.py
+++ sixerrapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Profile, Gig, Purchase, Review, Company
 
-# Register your models here.
+# Register your models here..
 admin.site.register(Profile)
 admin.site.register(Gig)
 admin.site.register(Purchase)
