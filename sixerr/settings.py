7bab098616e743bedbbab39127b0d58cfdf33d14
ANHTUAN DANG <tuanledang1120@gmail.com>
2019-02-14 14:25:18 -0800

    prod

--- sixerr/settings.py
+++ sixerr/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sixerrapp',
-    'social_django'
+    'social_django',
+    'storages'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -143,7 +144,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+
+AWS_ACCESS_KEY_ID = 'AKIAI5LF3MSVQEWM7GQA'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'cloud-cube'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
+# AWS_LOCATION = 'static'
+#
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'mysite/static'),
+# ]
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # to serve static files on Heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
