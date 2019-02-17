2406b41b78a09a405e52dc34065a4e5c6dec1671
ANHTUAN DANG <tuanledang1120@gmail.com>
2019-02-16 19:38:52 -0800

    checking in

--- sixerr/settings.py
+++ sixerr/settings.py
@@ -201,7 +201,7 @@ DATABASES['default'].update(db_from_env)
 
 # setup upload directory for Gig model
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'https://cloud-cube.s3.amazonaws.com/ov6lbnrabqfyov6lbnrabqfy/public/media/'
+MEDIA_URL = 'https://cloud-cube.s3.amazonaws.com/ov6lbnrabqfyov6lbnrabqfy/public/'
 
 #SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 SECURE_SSL_REDIRECT=False
