3d6dca404d623adcabf5d680a8da73efa8585301
Thong Vo <vo4@stolaf.edu>
2019-06-08 00:39:23 -0500

    Fix bug in reupload resume and add hover effect

--- sixerrapp/forms.py
+++ sixerrapp/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import Gig
+from .models import Gig, Profile
 
 class GigForm(ModelForm):
     class Meta:
         model = Gig
         fields = ['title', 'category', 'description', 'photo', 'status']
+
+class ProfileForm(ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['resume']
